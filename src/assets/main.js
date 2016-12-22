dfdf0a659b379c51e40ea62fa9e1a7e0bc8edf61
AchPanCS <a.p@a.nl>
2016-12-22 15:18:47 +0100

    First attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2906391.json',
+    datatype: 'jsonp',
+    success: function(response){
+        addCourses(response.courses.completed);
+      }
+    });
 
-  // your code will go here
+  function addCourses(courses) {
+    var badges = $('#badges');
+    courses.forEach(function(course) {
+      var course = $('<div />', {'class': 'course'}).appendTo(badges);
+      $('<h3 />', {text: course.title}).appendTo(course);
+      $('<img />', {src: course.badge}).appendTo(course);
+      $('<a />', {href: course.url, target:'_blank', 'class':'btn btn-primary', text: 'See course'}).appendTo(course);
+    })
+  };
 
 });
