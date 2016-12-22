1d20b30d431552a0e432f68ed34cf878c181d042
AchPanCS <a.p@a.nl>
2016-12-22 15:27:29 +0100

    3

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,10 +10,10 @@ $(function() {
   function addCourses(courses) {
     var badges = $('#badges');
     courses.forEach(function(course) {
-      var course = $('<div />', {'class': 'course'}).appendTo(badges);
-      $('<h3 />', {text: course.title}).appendTo(course);
-      $('<img />', {src: course.badge}).appendTo(course);
-      $('<a />', {href: course.url, target:'_blank', 'class':'btn btn-primary', text: 'See course'}).appendTo(course);
+      var $course = $('<div />', {'class': 'course'}).appendTo(badges);
+      $('<h3 />', {text: course.title}).appendTo($course);
+      $('<img />', {src: course.badge}).appendTo($course);
+      $('<a />', {href: course.url, target:'_blank', 'class':'btn btn-primary', text: 'See course'}).appendTo($course);
     })
   };
 
